8c3d812a332182c13b15bab5e92ffb7b1085d96b
Paulo-sudo <php_paulo@hotmail.com>
2022-04-26 12:22:03 -0300

    teste

--- routes.js
+++ routes.js
@@ -12,6 +12,9 @@ routes.options("*", cors(corsOptions));
 const news = require("./controllers/newsController");
 const weather = require("./controllers/weatherController");
 
+routes.get("/",cors(corsOptions),async function(req, res){
+    res.send({status:"ok"})
+})
 routes.get("/weather", cors(corsOptions), weather.Get);
 routes.get("/news", cors(corsOptions), news.Get);
 module.exports = routes;
